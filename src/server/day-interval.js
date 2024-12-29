f6b9644c16a84c3d0ddda539e7d7b0c6c2dcdd66
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-12-29 16:29:53 +0800

    fixed bug

--- src/server/day-interval.js
+++ src/server/day-interval.js
@@ -1,10 +1,10 @@
 function day_interval() {
 
-    var date2 = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate())
+    var date1 = new Date()
 
     var date1_tomorrow = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate() + 1);
     
-    if (date1_tomorrow.getFullYear() == date2.getFullYear() && date1_tomorrow.getMonth() == date2.getMonth() && date1_tomorrow.getDate() == date2.getDate()) {
+    if (date1_tomorrow.getFullYear() == date1.getFullYear() && date1_tomorrow.getMonth() == date1.getMonth() && date1_tomorrow.getDate() == date1.getDate()) {
         return true; // date2 is one day after date1.
     }
     else {

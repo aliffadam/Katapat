93a206676fcc9010873b82467143dcb31a32cdaf
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-12-29 18:36:47 +0800

    post and get

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const gameRouter = express.Router();
 
-const { game_data } = require('../db/client.js');
+const { game_data, word_today } = require('../db/client.js');
 
 const { jwt_search } = require('../utils/jwt-search.js')
 const { verify_jwt } = require('../utils/verify-jwt.js');
@@ -9,12 +9,66 @@ const { input_word } = require('../utils/input-word.js');
 
 gameRouter.route('/play')
     .post(verify_jwt, jwt_search, input_word, async (req, res) => {
-        let word = res.locals.word
-
         
+        let word = res.locals.word
+        let account = res.locals.account
+
+        let user = await game_data.findOne(
+            {
+                _id: account._id
+            }
+        )
+
+        if(user) {
+            if(user.successful || user.attempt >= 5) {
+                res.status(200).send("You've already finished playing.\nPlay again tomorrow!")
+                return
+            }
+        }
+
+        let answer = await word_today.findOne()
+
+        if(word == answer.word) {
+            let success = await game_data.updateOne(
+                { _id: account._id },
+                {
+                    $set: {
+                        successful: true
+                    }
+                },
+                { upsert: true }
+            )
+        }
+
+        //if unsuccessful
+        let increment_attempt = await game_data.updateOne(
+            { _id: account._id },
+            {
+                $inc: {
+                    attempt: 1
+                }
+            },
+            { upsert: true } //to add if not already exist
+        )
+
+        res.status(200).send('Please try again')
+
     })
     .get(verify_jwt, jwt_search, async (req, res) => {
-        res.status(204).send('Nothing here')
+
+        let account = res.locals.account
+
+        let user = await game_data.findOne(
+            {
+                _id: account._id
+            }
+        )
+
+        if(!user) {
+            res.status(200).send('You have not played yet.\nStart playing now')
+        }
+
+        res.status(200).send(user)
     })
     .patch(verify_jwt, jwt_search, async (req, res) => {
         res.status(204).send('Nothing here')

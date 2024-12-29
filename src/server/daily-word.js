5416f5c9d0fc342755a9c9b84946ac11fac419df
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-12-29 16:15:59 +0800

    created daily-word.js
    but need to add later rechecks to retry

--- src/server/daily-word.js
+++ src/server/daily-word.js
@@ -1,9 +1,25 @@
+const { word_today, word_list } = require('../db/client')
+
 const { day_interval } = require('./day-interval')
 
-function daily_word() {
+async function daily_word() {
 
     if(day_interval()) {
         //randomly pick a new word
+
+        let remove_old = await word_today.deleteMany()
+
+        //TODO should create checks to retry
+
+        let list_words = await word_list.find().toArray()
+
+        const chosen_word = list_words[Math.floor(Math.random() * list_words.length)];
+
+        let insert_new = await word_today.insertOne(
+            {
+                word: chosen_word
+            }
+        )
     }
 
 }
